dceaccfe7ae267a480c2b32ef345770b01efba2b
Nicmo-Dev <nicmocontatoprofissional@gmail.com>
2026-02-08 01:05:25 -0300

    Add task persistence with localStorage

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function saveTasks() {
+  const tasks = [];
+
+  document.querySelectorAll("li").forEach(li => {
+    tasks.push(li.innerText);
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 function addTask() {
   const input = document.getElementById("taskInput");
   const taskText = input.value;
@@ -12,8 +22,28 @@ function addTask() {
 
   li.onclick = function () {
     li.remove();
+    saveTasks();
   };
 
   document.getElementById("taskList").appendChild(li);
   input.value = "";
+
+  saveTasks();
+}
+
+/* Carregar tarefas salvas quando o site abre */
+const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+if (savedTasks) {
+  savedTasks.forEach(task => {
+    const li = document.createElement("li");
+    li.innerText = task;
+
+    li.onclick = function () {
+      li.remove();
+      saveTasks();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+  });
 }
